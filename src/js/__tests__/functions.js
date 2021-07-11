4e03c9454912dc5e87e9bc21dfd50d95c5710695
Mike <maumike87@gmail.com>
2021-07-10 20:52:21 -0400

    feat: create generateRowsAndColumns() with initial tests

---
+++ src/js/__tests__/functions.js
@@ -0,0 +1,31 @@
+import { generateRowsAndColumns } from '../functions'
+
+describe('generateRowsAndColumns', () => {
+  const randomDimension = () => {
+    const num = Math.round(
+      Math.random() * (Math.round(Math.random()) ? 10 : 100)
+    )
+    return num
+  }
+  let width
+  let height
+  let grid
+
+  beforeEach(() => {
+    width = randomDimension()
+    height = randomDimension()
+    grid = generateRowsAndColumns(width, height)
+  })
+
+  test('Returns a 2-dimensional array', () => {
+    expect(grid).toBe(grid)
+  })
+  test('The main array has a length equal to the given width', () => {
+    expect(grid).toHaveLength(width)
+  })
+  test('The subarrays have a length equal to the given height', () => {
+    for (let i = 0; i < grid.length; i++) {
+      expect(grid[i]).toHaveLength(height)
+    }
+  })
+})
---
+++ src/js/functions.js
@@ -0,0 +1,13 @@
+// I want a grid that can have a custom number of rows and columns, selected 
+// independently from one another. It must return a 2-dimensional array of 0's.
+// The main array will be the width, and the subarray will be the height.
+export const generateRowsAndColumns = (width, height) => {
+  const grid = new Array(width)
+  for (let i = 0; i < grid.length; i++) {
+    grid[i] = new Array(height)
+    for (let j = 0; j < grid[i].length; j++) {
+      grid[i][j] = 0
+    }
+  }
+  return grid
+}
